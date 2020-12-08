1c00fc459b5f866c5453acd3aee8c5e6c06603da
Cherish Collins <cherish.ebojudah@gmail.com>
2020-12-08 20:01:19 +0100

    created landing page for floop website

---
+++ js/function.js
@@ -0,0 +1,24 @@
+// // const displayArray = (numArr, num) => {
+// //     numArr.push(num)
+// //     return numArr;
+// // }
+
+// // console.log(displayArray('hello', 4));
+
+
+// let student = {
+//     age: 16,
+//     firstName: 'Cherish',
+//     lastName: 'Collins',
+//     hobby: 'Reading books',
+//     peHouse: 'Yellow house',
+//     classTeacher: { firstName: 'Uncle',
+//                     lastName: 'Doyin',
+//                     age: 20
+//     }
+// }
+
+// let age = student.age;
+// let teacherAge = student.classTeacher.age;
+// console.log(teacherAge);
+// console.log(age);
